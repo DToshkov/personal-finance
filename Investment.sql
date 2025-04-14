2300813137e2bb39260589dd003e4aaf41fb573e
Dimitar <109114920+DToshkov@users.noreply.github.com>
2025-04-13 19:47:29 -0500

    Create Investment.sql
    create Investment table in phpMyAdmin

---
+++ Investment.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Investment (
+    investment_ID int NOT NULL,
+    user_id int NOT NULL,
+    type varchar(50) NOT NULL,
+    institution varchar(50) NOT NULL,
+    date_received date,
+    ROI decimal(9,2) NOT NULL,
+    account_deposit_id int NOT NULL,
+    recurring boolean,
+    associated_account varchar(50) NOT NULL,
+    vendor varchar(50) NOT NULL,
+    PRIMARY KEY investment_ID,
+    FOREIGN KEY (user_id) REFERENCES User_Details(user_id)
+    );
+    
